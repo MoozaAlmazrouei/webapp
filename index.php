cdd3ba2fd14c5e337fb415fcf5b5782b9d5e44ce
MoozaAlmazrouei <64743633+MoozaAlmazrouei@users.noreply.github.com>
2020-05-03 22:05:50 +0530

    My PHP Web Application
    My PHP Web Application

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>My PHP Web Application </h2>";
+echo "Hello world!<br>";
+echo "I have create this web application using azure web app services<br>";
+?> 
+
+</body>
+</html>
